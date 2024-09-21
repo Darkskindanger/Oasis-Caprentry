c94ea5847d11198ec2d01e2245b175cf864f2fc2
Darkskindanger <kiliantavareswork@gmail.com>
2024-09-21 21:37:51 +1000

    minor fixes prior to hosting

--- src/_data/client.js
+++ src/_data/client.js
@@ -1,21 +1,21 @@
 module.exports = {
-    "name": "Oasis carpentry",
-    "email": "Mo@Oasis.com.au",
-    "phoneForTel": "0400000000",
-    "phoneFormatted": "(61) 400-000000",
-    "address": {
-        // "lineOne": "N/A",
-        // "lineTwo": "N/A",
-        "city": "Sydney",
-        "state": "NSW",
-        "zip": "2000",
-        "country": "AU",
-        "mapLink": "https://goo.gl/maps/UAQn4vuGDiwv7DV39"
-    },
-    "socials": {
-        "facebook": "https://www.facebook.com/",
-        "instagram": "https://www.instagram.com/",
-    },
-    //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
-    "domain": "https://www.example.com"
+  name: "Oasis carpentry",
+  email: "Mo@Oasis.com.au",
+  phoneForTel: "0404912314",
+  phoneFormatted: "(61) 404912314",
+  address: {
+    lineOne: "N/A",
+    lineTwo: "N/A",
+    city: "Sydney",
+    state: "NSW",
+    zip: "2000",
+    country: "AU",
+    mapLink: "https://goo.gl/maps/UAQn4vuGDiwv7DV39",
+  },
+  socials: {
+    facebook: "https://www.facebook.com/",
+    instagram: "https://www.instagram.com/",
+  },
+  //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
+  domain: "https://www.example.com",
 };
